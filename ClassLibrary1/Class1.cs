bf81d9bfc623934e74f9ed9d6c3c03c47a2eac80
Vitaly Rymarev <v.rymarev@a1qa.com>
2016-06-05 22:42:12 +0300

    second_task_executed

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public int FirstNumber { set; private get; }
+        public int SecondNumber { set; private get; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+    }
+}
